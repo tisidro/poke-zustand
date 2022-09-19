d2793bf2ae9dae40238f640f95dcec451b090a7f
Terri Isidro <terri.isidro@gmail.com>
2022-09-18 21:18:50 -0400

    feat: pull pokemon from gist data
    add gist URL, invoke Zustand hooks for filter and setPokemon in App
    component and create useEffect to fetch pokemon from the gist URL.
    Create table with Zustand useStore hook for pokemon that uses id to
    display associated name and type of pokemon that are retrieved via map
    method on the pokemon const.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import create from 'zustand'
 import './App.css'
 
+// link to gist with all pokemon
+const POKE_URL =
+  'https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/0658aeff401d196dece7ec6fe6c726c6adc1cc00/gistfile1.txt'
+
 // create useStore hook
 const useStore = create(set => ({
   filter: '',
@@ -31,14 +35,41 @@ const FilterInput = () => {
 function App () {
   // filter hook invocation
   const filter = useStore(state => state.filter)
+  const setPokemon = useStore(state => state.setPokemon)
+  // const pokemon = useStore(state => state.pokemon)
+
+  React.useEffect(() => {
+    fetch(POKE_URL)
+      .then(res => res.json())
+      .then(pokemon => setPokemon(pokemon))
+  })
+
+  const PokemonTable = () => {
+    const pokemon = useStore(state => state.pokemon)
+
+    return (
+      <table width='95%'>
+        <tbody>
+          {pokemon.map(({ id, name: { english }, type }) => (
+            <tr key={id}>
+              <td> {english}</td>
+              <td> {type.join(',')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
+
   return (
     <div className='App'>
       <div>
         {/* call component for when someone filters the pokemon data */}
         <FilterInput />
+        <PokemonTable />
       </div>
       {/* add value of filter so we can see if it's returning--it works! */}
-      {filter}
+      {/* {JSON.stringify()} */}
     </div>
   )
 }
