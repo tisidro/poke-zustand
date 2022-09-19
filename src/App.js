8206718f61429e9f21a66e9b908a502fa711be31
Terri Isidro <terri.isidro@gmail.com>
2022-09-18 20:17:12 -0400

    feat: set states with useStore hook
    write useStore hook, set states for filter and pokemon, create FilterInput
    component for App function that sets states for filter and setFilter
    when someone types a pokemon in the blank field.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import create from 'zustand'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// create useStore hook
+const useStore = create(set => ({
+  filter: '',
+  pokemon: [],
+  // set takes a state for filter and returns a new state
+  setFilter: filter =>
+    set(state => ({
+      ...state,
+      filter
+    })),
+  //take in state for pokemon and return new state
+  setPokemon: pokemon =>
+    set(state => ({
+      ...state,
+      pokemon
+    }))
+}))
+
+// Filter input component - returns input
+const FilterInput = () => {
+  const filter = useStore(state => state.filter)
+  const setFilter = useStore(state => state.setFilter)
+  // call setFilter when there's an event
+  return <input value={filter} onChange={evt => setFilter(evt.target.value)} />
 }
 
-export default App;
+function App () {
+  // filter hook invocation
+  const filter = useStore(state => state.filter)
+  return (
+    <div className='App'>
+      <div>
+        {/* call component for when someone filters the pokemon data */}
+        <FilterInput />
+      </div>
+      {/* add value of filter so we can see if it's returning--it works! */}
+      {filter}
+    </div>
+  )
+}
+
+export default App
